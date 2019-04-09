4f7adae32e45a94cc3007e28a954716a17d4ead8
mutster <alex@scotland.ca>
2019-04-09 17:12:57 -0400

    Tempo V.33.1
    First iteration of Tempo.  Working notes in key.

---
+++ discord_launcher.py
@@ -0,0 +1,81 @@
+##HEELLOO U NERDS
+import discord
+from discord.ext import commands
+import random
+##THIS IS WHERE WE MAKE LISTS OF KEYS
+key_C = ['C','D','E','F','G','A','B']
+noteList = ['C','Db','D','Eb','E','F','Gb','G','Ab','A','Bb','B','C','Db','D','Eb','E','F','Gb','G','Ab','A','Bb','B']
+### Minors 2 1 2 2 1 2 2
+### Majors 2 2 1 2 2 2 1
+description = '''lel oidk what im doiog :D'''
+bot = commands.Bot(command_prefix='#', description=description)
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+@bot.command()
+async def key(ctx, note:str,length=1):
+    """Bot takes argument,
+applies to notes, then gives back notes that sound aight"""
+    if length < 1:
+        length = 1
+    elif length > 7:
+        length = 7
+    if 'm' in note:
+        tempnote = note[:2]
+        print(len(note))
+    else:
+        tempnote = note
+    if tempnote in noteList:
+        rList = []
+        if 'm' not in note:
+            print('major')
+            print(tempnote)
+            setpos = noteList.index(tempnote)
+            counter = 0
+            number = setpos
+            if length == 1:
+                if 'b' in noteList[number+1]:
+                    rList.append(noteList[number+2])
+                else:
+                    rList.append(noteList[number+2])
+            else:
+                while counter != length:
+                    if counter == 2 or counter == 6:
+                        number += 1
+                        rList.append(noteList[number])
+                        counter += 1
+                    else:
+                        number += 2
+                        rList.append(noteList[number])
+                        counter += 1
+        else:
+            print('minor')
+            print(tempnote)
+            setpos = noteList.index(tempnote)
+            counter = 0
+            number = setpos
+            if length == 1:
+                if 'b' in noteList[number+1]:
+                    rList.append(noteList[number+2])
+                else:
+                    rList.append(noteList[number+2])
+            else:
+                while counter != length:
+                    if counter == 1 or counter == 4:
+                        number += 1
+                        rList.append(noteList[number])
+                        counter += 1
+                    else:
+                        number += 2
+                        rList.append(noteList[number])
+                        counter += 1
+
+        solution = 'Next note in this key is:  '+str(rList)
+    else:
+        solution = "Couldn't find your Note: "+note+", please remember, the syntax is #notes note key length"
+    await ctx.send(solution)
+bot.run('botkeygoeshere')
