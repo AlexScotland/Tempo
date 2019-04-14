2723f9c0a7179c3153c9e9dbcadd3586c8d454f2
Dug <alex@scotland.ca>
2019-04-13 22:23:12 -0400

    added voice features to progression command

--- README.md
+++ README.md
@@ -8,12 +8,19 @@ Tempo will show you (and any users) different Musical Theory for whichever note 
 
 Requirements:
  - Discord.py
+ - Discord.py[voice]
+ - opuslib
  - Python 3.7.3
  
  Usage
-  - #Key <Note> <length>
+  - #Key <Key> <length>
     - Note - The note you are starting on:  Cm / C
     - Length - How many notes you want shown (default 1, max 7)
+  - #prog <note> <length>
+    - Key - The Key you want
+    - Length - How long is your progression?
+	#prog also has Voice chat support!
+		- if user is in voice chat, Tempo will join and play your notes to you!
    
 Too add Tempo to your server, use the following link:
-https://discordapp.com/api/oauth2/authorize?client_id=564959346165809159&permissions=0&scope=bot
+https://discordapp.com/api/oauth2/authorize?client_id=564959346165809159&permissions=36760832&scope=bot
--- discord_launcher.py
+++ discord_launcher.py
@@ -1,38 +1,55 @@
-##HEELLOO U NERDS
 import discord
 from discord.ext import commands
-import random
-## Flippity Floppity, hers your note-opity
+from discord.voice_client import VoiceClient
+import os, random, asyncio, time
 global noteList
-noteList = ['C','Db','D','Eb','E','F','Gb','G','Ab','A','Bb','B','C','Db','D','Eb','E','F','Gb','G','Ab','A','Bb','B']
+noteList = ['C','C#','D','D#','E','F','F#','G','G#','A','A#','B','C','C#','D','D#','E','F','F#','G','G#','A','A#','B']
 global minor
 minor = [2,1,2,2,1,2,2]
 global major
 major = [2,2,1,2,2,2,1]
 
 def keys(note, length):
-    fuck = []
+    key_list = []
     if "m" in note:
-        note = note[:note.index("m")] 
+        note = note[:note.index("m")]
         count = noteList.index(note)
         if length == 1:
-            fuck.append(noteList[count+2])
-        else:    
+            key_list.append(noteList[count+2])
+        else:
             for x in minor[:length]:
-                fuck.append(noteList[count])
+                key_list.append(noteList[count])
                 count += x
     else:
         count = noteList.index(note)
         if length == 1:
-            fuck.append(noteList[count+2])
-        else:    
+            key_list.append(noteList[count+2])
+        else:
             for x in major[:length]:
-                fuck.append(noteList[count])
+                key_list.append(noteList[count])
                 count += x
-    return fuck
+    return key_list
+
+def progression(keylist,amt):
+    answer_list=[]
+    if amt <= 0:
+        amt = 3
+    elif amt >=7:
+        amt = 7
+    counter = 1
+    while counter <= amt:
+        potential =random.randint(0,6)
+        if keylist[potential] in answer_list:
+            pass
+        else:
+            answer_list.append(keylist[potential])
+            counter += 1
+    return answer_list
+
 
 description = '''lel idk what im doing :D'''
-bot = commands.Bot(command_prefix='#', description=description)
+bot = commands.Bot(command_prefix='#')
+soundList = os.listdir('sounds/')
 @bot.event
 async def on_ready():
     print('Logged in as')
@@ -48,8 +65,24 @@ applies to notes, then gives back notes that sound aight"""
         length = 1
     elif length > 7:
         length = 7
-        
     answer=keys(note, length)
     solution = 'Next note in this key is:  '+str(answer)
     await ctx.send(solution)
-bot.run('NTY0OTU5MzQ2MTY1ODA5MTU5.XKvu6A.HQcTacWF1pA6GznSze6c596I68Y')
+
+@bot.command()
+async def prog(ctx, note:str,amount=3):
+    """Bot takes the key, and amount then generates a random chord progression."""
+    k=keys(note,7)
+    answer = progression(k,amount)
+    solution = 'Generated Key Progression '+str(answer)
+    await ctx.send(solution)
+    if ctx.author.voice is not None:
+        vc = await ctx.author.voice.channel.connect()
+        for i in range(len(answer)):
+            source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio('sounds/'+str(answer[i])+'.mp3'))
+            ctx.voice_client.play(source, after=lambda e: print('Player error: %s' % e) if e else None)
+            time.sleep(1)
+        await vc.disconnect()
+
+
+bot.run('')
